e57483b197636ed858b2741d919cdef6f9aeb537
Mike Weliczko <michael.weliczko@portlandwebworks.com>
2023-03-21 22:27:15 -0500

    removed unused parameterized test because we only need these cases

--- src/test/java/org/testdouble/AccountNumberTest.java
+++ src/test/java/org/testdouble/AccountNumberTest.java
@@ -1,22 +1,20 @@
 package org.testdouble;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountNumberTest {
 
-  @ParameterizedTest
-  @ValueSource(strings = {"000000051"})
-  public void isValid(String validValue) {
+  @Test
+  public void isValid() {
+    final String validValue= "000000051";
     assertEquals(validValue, new AccountNumber(validValue).toString());
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = {"000000057"})
-  public void notValid(String invalidValue) {
+  @Test
+  public void notValid() {
+    final String invalidValue = "000000057";
     assertEquals(invalidValue + " ERR", new AccountNumber(invalidValue).toString());
   }
 
