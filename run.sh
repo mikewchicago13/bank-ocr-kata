c0b5e22047f9a926b7b264831659336375cf078e
Mike Weliczko <michael.weliczko@portlandwebworks.com>
2023-03-21 20:45:43 -0500

    update compiler to use features in more advanced versions of java

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 mvn clean test package
 
-java -cp target/bank-ocr-kata-1.0-SNAPSHOT.jar org.testdouble.App $(pwd)/input.txt
+java -cp target/bank-ocr-kata-1.0-SNAPSHOT.jar org.testdouble.App "$(pwd)/input.txt"
